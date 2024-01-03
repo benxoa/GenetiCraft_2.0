a0cbacfe6fee74360e26f48c8d747ec3df4979fb
benxoa <143406036+benxoa@users.noreply.github.com>
2024-01-03 22:16:11 +0500

    Update PdfTranslator.jsx

--- client/src/tools/PdfTranslator.jsx
+++ client/src/tools/PdfTranslator.jsx
@@ -207,51 +207,6 @@ const PdfTranslator = () => {
         <link rel="canonical" href="https://www.geneticraft.fun/translate-pdf" />
       </Helmet>
 
-      <section className="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center py-12 ">
-  <div className="w-full max-w-[58rem] bg-white rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700 sm:p-4 lg:p-6 m-4">
-    <div className="p-6 space-y-4 ">
-      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white text-center ">
-        PDF TO TEXT
-      </h1>
-    
-      <form className="space-y-4" onSubmit={uploadFile}>
-        <div>
-          <label
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            htmlFor="file_input"
-          >
-            Upload file
-          </label>
-          <input
-            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            aria-describedby="file_input_help"
-            id="file_input"
-            type="file"
-            onChange={(e) => setFile(e.target.files[0])}
-          />
-          <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help"></p>
-        </div>
-        <button
-          type="submit"
-          className="w-full text-white bg-blue-300 hover:bg-black focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          Convert To Text
-        </button>
-        {text && (
-          <>
-            <div className="block p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 overflow-auto h-[400px]">
-              {text}
-            </div>
-            <button type="button" onClick={copyText} className="bg-green-400 rounded-md w-full text-white py-2">
-              Copy Text
-            </button>
-          </>
-        )}
-      </form>
-    </div>
-  </div>
-</section>
-
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
